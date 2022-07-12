4a408200ca89657c5193df2c8e47eefb3647e68c
Avni Dosi <71483459+golu2molu@users.noreply.github.com>
2022-07-12 15:14:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+setInterval(() =>{
+    d=new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hrot=30*htime+mtime/2;
+    mrot=6*mtime;
+    srot=6*stime;
+    document.getElementById("my_audio").play();
+    hour.style.transform=`rotate(${hrot}deg)`;
+    minute.style.transform=`rotate(${mrot}deg)`;
+    second.style.transform=`rotate(${srot}deg)`;
+
+},1000);
+
+
+function displayTime(){
+    document.getElementById("text").innerHTML=htime+":"+mtime+":"+stime;
+    
+}
